0426270d6f8556efe8f6487a490230bc55fd1d9a
PeterB <peterjbishop.denver@gmail.com>
2025-03-09 03:15:27 -0600

    end to end test of user routes PASS

--- server/server_test.go
+++ server/server_test.go
@@ -5,6 +5,7 @@ import (
 	"database/sql"
 	"encoding/json"
 	"fmt"
+	"free-adventure-go/main.go/postgres/queries"
 	"free-adventure-go/main.go/server/routes"
 	"log"
 	"net/http"
@@ -33,7 +34,7 @@ func TestHello(t *testing.T) {
 	})
 }
 
-func ConnectForTest() (*sql.DB, error) {
+func ConnectDB() (*sql.DB, error) {
 	err := godotenv.Load("../.env")
 	if err != nil {
 		log.Fatal("Failed to load .env file:", err)
@@ -63,15 +64,105 @@ func ConnectForTest() (*sql.DB, error) {
 	return db, nil
 }
 
-func TestGetUsers(t *testing.T) {
-	t.Run("returns a success message", func(t *testing.T) {
+func TestUserEndpoints(t *testing.T) {
+
+	var user queries.User
+	user.Name = "Test User 2"
+	user.Email = "test 2@gmail.com"
+	user.Password = "myTestPassword"
+
+	t.Run("creates a user", func(t *testing.T) {
+		fmt.Println("Creating a user...")
+		requestBody, _ := json.Marshal(map[string]string{
+			"name":     user.Name,
+			"email":    user.Email,
+			"password": user.Password,
+		})
+
+		request, _ := http.NewRequest(http.MethodPost, "/register", bytes.NewBuffer(requestBody))
+		response := httptest.NewRecorder()
+
+		db, err := ConnectDB()
+		if err != nil {
+			t.Fatal("Failed to connect to database:", err)
+		}
+		defer db.Close()
+
+		routes.CreateUserHandler(db, response, request)
+
+		var got map[string]interface{}
+		err = json.Unmarshal([]byte(response.Body.Bytes()), &got)
+		if err != nil {
+			t.Fatal("Failed to unmarshal response body:", err)
+		}
+		want := "User Created Successfully"
+		if msg, ok := got["message"].(string); !ok || msg != want {
+			t.Errorf(`Expected message: "%s", but got: "%v"`, want, got["message"])
+		} else {
+			fmt.Println("PASS")
+		}
+
+	})
+
+	t.Run("user logged in", func(t *testing.T) {
+		fmt.Println("Logging in a user...")
+		requestBody, _ := json.Marshal(map[string]string{
+			"email":    user.Email,
+			"password": user.Password,
+		})
+
+		request, _ := http.NewRequest(http.MethodPost, "/login", bytes.NewBuffer(requestBody))
+		response := httptest.NewRecorder()
+
+		db, err := ConnectDB()
+		if err != nil {
+			t.Fatal("Failed to connect to database:", err)
+		}
+		defer db.Close()
+
+		routes.Login(db, response, request)
+
+		var got map[string]interface{}
+		err = json.Unmarshal([]byte(response.Body.Bytes()), &got)
+		if err != nil {
+			t.Fatal("Failed to unmarshal response body:", err)
+		}
+
+		userData, ok := got["user"].(map[string]interface{})
+		if !ok {
+			t.Fatal("Failed to parse user data from login response")
+		}
+
+		id, ok := userData["id"].(string)
+		if !ok {
+			t.Fatal("Failed to parse user ID from login response")
+		}
+
+		user.ID = id
+
+		pass, ok := userData["password"].(string)
+		if !ok {
+			t.Fatal("Failed to parse user password from login response")
+		}
+		user.Password = pass
+
+		want := "Login Success"
+		if msg, ok := got["message"].(string); !ok || msg != want {
+			t.Errorf(`Expected message: "%s", but got: "%v"`, want, got["message"])
+		} else {
+			fmt.Println("PASS")
+		}
+	})
+
+	t.Run("finds all users", func(t *testing.T) {
+		fmt.Println("Finding all users...")
 		request, err := http.NewRequest(http.MethodGet, "/users/", nil)
 		if err != nil {
 			t.Fatal("Failed to create a new request:", err)
 		}
 		response := httptest.NewRecorder()
 
-		db, err := ConnectForTest()
+		db, err := ConnectDB()
 		if err != nil {
 			t.Fatal("Failed to connect to database:", err)
 		}
@@ -87,37 +178,129 @@ func TestGetUsers(t *testing.T) {
 		want := "Users found!"
 		if msg, ok := got["message"].(string); !ok || msg != want {
 			t.Errorf(`Expected message: "%s", but got: "%v"`, want, got["message"])
+		} else {
+			fmt.Println("PASS")
 		}
 	})
-}
 
-func TestLogin(t *testing.T) {
-	t.Run("returns a success message", func(t *testing.T) {
-
-		requestBody, _ := json.Marshal(map[string]string{
-			"email":    "test.middleware@gmail.com",
-			"password": "myPassword",
-		})
-
-		request, _ := http.NewRequest(http.MethodPost, "/login/", bytes.NewBuffer(requestBody))
+	t.Run("finds a user by id", func(t *testing.T) {
+		fmt.Println("Finding a user by ID...")
+		request, err := http.NewRequest(http.MethodGet, "/users/id/"+user.ID, nil)
+		if err != nil {
+			t.Fatal("Failed to create a new request:", err)
+		}
 		response := httptest.NewRecorder()
 
-		db, err := ConnectForTest()
+		db, err := ConnectDB()
 		if err != nil {
 			t.Fatal("Failed to connect to database:", err)
 		}
 		defer db.Close()
 
-		routes.Login(db, response, request)
+		routes.GetUserByIDHandler(db, response, request, user.ID)
 
 		var got map[string]interface{}
 		err = json.Unmarshal([]byte(response.Body.Bytes()), &got)
 		if err != nil {
 			t.Fatal("Failed to unmarshal response body:", err)
 		}
-		want := "Login Success"
+		want := "User Found with ID, " + user.ID
 		if msg, ok := got["message"].(string); !ok || msg != want {
 			t.Errorf(`Expected message: "%s", but got: "%v"`, want, got["message"])
+		} else {
+			fmt.Println("PASS")
+		}
+	})
+
+	t.Run("finds a user by email", func(t *testing.T) {
+		fmt.Println("Finding a user by email...")
+		request, err := http.NewRequest(http.MethodGet, "/users/email/"+user.Email, nil)
+		if err != nil {
+			t.Fatal("Failed to create a new request:", err)
+		}
+		response := httptest.NewRecorder()
+
+		db, err := ConnectDB()
+		if err != nil {
+			t.Fatal("Failed to connect to database:", err)
+		}
+		defer db.Close()
+
+		routes.GetUserByEmailHandler(db, response, request, user.Email)
+
+		var got map[string]interface{}
+		err = json.Unmarshal([]byte(response.Body.Bytes()), &got)
+		if err != nil {
+			t.Fatal("Failed to unmarshal response body:", err)
+		}
+		want := "User Found with Email, " + user.Email
+		if msg, ok := got["message"].(string); !ok || msg != want {
+			t.Errorf(`Expected message: "%s", but got: "%v"`, want, got["message"])
+		} else {
+			fmt.Println("PASS")
+		}
+	})
+
+	t.Run("user updated", func(t *testing.T) {
+		fmt.Println("Updating a user...")
+		requestBody, _ := json.Marshal(map[string]string{
+			"name":       "Test Update User",
+			"email":      "test.updated@gmail.com",
+			"password":   user.Password,
+			"updated_at": "2021-09-01T00:00:00Z",
+			"created_at": "2021-09-01T00:00:00Z",
+		})
+
+		request, _ := http.NewRequest(http.MethodPut, "/users/update/"+user.ID, bytes.NewBuffer(requestBody))
+		response := httptest.NewRecorder()
+
+		db, err := ConnectDB()
+		if err != nil {
+			t.Fatal("Failed to connect to database:", err)
+		}
+		defer db.Close()
+
+		routes.UpdateUserHandler(db, response, request, user.ID)
+
+		var got map[string]interface{}
+		err = json.Unmarshal([]byte(response.Body.Bytes()), &got)
+		if err != nil {
+			t.Fatal("Failed to unmarshal response body:", err)
+		}
+		want := "User updated!"
+		if msg, ok := got["message"].(string); !ok || msg != want {
+			t.Errorf(`Expected message: "%s", but got: "%v"`, want, got["message"])
+		} else {
+			fmt.Println("PASS")
+		}
+	})
+
+	t.Run("user deleted", func(t *testing.T) {
+		fmt.Println("Deleting a user...")
+		request, err := http.NewRequest(http.MethodDelete, "/users/delete/"+user.ID, nil)
+		if err != nil {
+			t.Fatal("Failed to create a new request:", err)
+		}
+		response := httptest.NewRecorder()
+
+		db, err := ConnectDB()
+		if err != nil {
+			t.Fatal("Failed to connect to database:", err)
+		}
+		defer db.Close()
+
+		routes.DeleteUserHandler(db, response, request, user.ID)
+
+		var got map[string]interface{}
+		err = json.Unmarshal([]byte(response.Body.Bytes()), &got)
+		if err != nil {
+			t.Fatal("Failed to unmarshal response body:", err)
+		}
+		want := "User deleted!"
+		if msg, ok := got["message"].(string); !ok || msg != want {
+			t.Errorf(`Expected message: "%s", but got: "%v"`, want, got["message"])
+		} else {
+			fmt.Println("PASS")
 		}
 	})
 }
