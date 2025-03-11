983969f54745db7d5b1d44aadec853346a11946f
PeterB <peterjbishop.denver@gmail.com>
2025-03-10 23:08:26 -0600

    tui ready for functionality

--- cli/boba/model.go
+++ cli/boba/model.go
@@ -1,19 +1,29 @@
 package boba
 
 import (
+	"github.com/charmbracelet/bubbles/spinner"
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 )
 
 type model struct {
-	greeting string
+	options  []string
+	cursor   int
+	selected map[int]struct{}
+	spinner  spinner.Model
 }
 
 func InitialModel() model {
+	s := spinner.New()
+	s.Spinner = spinner.Dot
+	s.Style = lipgloss.NewStyle().Foreground(lipgloss.Color("205"))
 	return model{
-		greeting: "Hello, World!",
+		options:  []string{"Users", "Items", "Orders", "Invoices", "Payments"},
+		selected: make(map[int]struct{}),
+		spinner:  s,
 	}
 }
 
 func (m model) Init() tea.Cmd {
-	return nil
+	return tea.Batch(m.spinner.Tick)
 }
--- cli/boba/view.go
+++ cli/boba/view.go
@@ -1,5 +1,74 @@
 package boba
 
+import (
+	"fmt"
+)
+
+func QueryUsers() string {
+	return "Finding all users!"
+}
+
+func QueryItems() string {
+	return "Finding all items!"
+}
+
+func QueryOrders() string {
+	return "Finding all orders!"
+}
+func QueryInvoices() string {
+	return "Finding all invoices!"
+}
+func QueryPayments() string {
+	return "Finding all payments!"
+}
+
 func (m model) View() string {
-	return m.greeting
+	// The header
+	s := "\n\nFETCH:\n\n"
+
+	// Iterate over our choices
+	for i, choice := range m.options {
+
+		// Is the cursor pointing at this choice?
+		cursor := " " // no cursor
+		if m.cursor == i {
+			cursor = ">" // cursor!
+		}
+
+		// Is this choice selected?
+		checked := " " // not selected
+		if _, ok := m.selected[i]; ok {
+			checked = "x" // selected!
+		}
+
+		getting := ""
+		if _, ok := m.selected[i]; ok {
+			switch m.options[i] {
+			case "Users":
+				result := QueryUsers()
+				getting = ("-> " + m.spinner.View() + result)
+			case "Items":
+				result := QueryItems()
+				getting = ("-> " + m.spinner.View() + result)
+			case "Orders":
+				result := QueryOrders()
+				getting = ("-> " + m.spinner.View() + result)
+			case "Invoices":
+				result := QueryInvoices()
+				getting = ("-> " + m.spinner.View() + result)
+			case "Payments":
+				result := QueryPayments()
+				getting = ("-> " + m.spinner.View() + result)
+			}
+		} else {
+			getting = ""
+		}
+
+		s += fmt.Sprintf("%s [%s] %s %s\n", cursor, checked, choice, getting)
+	}
+
+	// The footer
+	s += "\nPress q to quit.\n"
+
+	return s
 }
